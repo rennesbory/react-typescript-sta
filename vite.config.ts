299e22a4f4439405cc8da4517e59cbbb23979e11
rennesbory <rennes81@gmail.com>
2025-08-10 00:22:32 -0700

    GitHub Pages 배포를 위한 base path 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 
-const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': resolve(projectRoot, 'src')
-    }
-  },
-});
+  base: '/react-typescript-sta/',
+})
